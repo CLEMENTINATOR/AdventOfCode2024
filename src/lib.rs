b9e51329e1a3471158acafe89e6a822a0676b665
Clement Bouvet <clement@cbouvet.fr>
2024-12-15 14:41:00 +0100

    day1: import

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@ mod utils;
 
 // #[allow(dead_code, unused_variables)]
 // mod day_template;
+pub mod day1;
--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,7 @@ fn run_day(day: u8) {
     }
 
     match day {
-        // 1 => run_day!(1),
+        1 => run_day!(1),
         // 2 => run_day!(2),
         // 3 => run_day!(3),
         // 4 => run_day!(4),
