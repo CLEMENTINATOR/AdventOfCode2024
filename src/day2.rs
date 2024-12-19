9d5a8698ead87bed762c3567aa185cdde6051cd7
Clement Bouvet <clement@cbouvet.fr>
2024-12-19 10:52:12 +0100

    day2: solve

--- src/day2.rs
+++ src/day2.rs
@@ -1,11 +1,82 @@
 type Num = u32;
 
+enum Order {
+    Asc,
+    Desc,
+}
+fn are_my_tokens_safe(tokens: Vec<u32>) -> bool {
+    let token_window = tokens.windows(2);
+    let mut ascending: Option<Order> = None;
+
+    for window in token_window {
+        match window {
+            &[first, second] => {
+                if first.abs_diff(second) == 0 || first.abs_diff(second) > 3 {
+                    return false;
+                }
+                match ascending {
+                    None => {
+                        ascending = if first > second {
+                            Some(Order::Desc)
+                        } else {
+                            Some(Order::Asc)
+                        }
+                    }
+                    Some(Order::Asc) => {
+                        if first > second {
+                            return false;
+                        }
+                    }
+                    Some(Order::Desc) => {
+                        if second > first {
+                            return false;
+                        }
+                    }
+                }
+            }
+            _ => {}
+        }
+    }
+    true
+}
+
+fn is_my_line_safe(line: &str) -> bool {
+    let tokens: Vec<u32> = line
+        .split_whitespace()
+        .map(|x| x.parse::<u32>().unwrap())
+        .collect();
+    are_my_tokens_safe(tokens)
+}
+
 pub fn part1(input: &str) -> Num {
-    0
+    input
+        .lines()
+        .map(|line| if is_my_line_safe(line) { 1 } else { 0 })
+        .sum()
 }
 
 pub fn part2(input: &str) -> Num {
-    0
+    input
+        .lines()
+        .map(|line| {
+            if is_my_line_safe(line) {
+                1
+            } else {
+                let tokens: Vec<u32> = line
+                    .split_whitespace()
+                    .map(|x| x.parse::<u32>().unwrap())
+                    .collect();
+                for i in 0..tokens.len() {
+                    let (left, right) = tokens.split_at(i);
+                    let temp_tokens = [left, &right[1..]].concat();
+                    if are_my_tokens_safe(temp_tokens) {
+                        return 1;
+                    }
+                }
+                0
+            }
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -13,13 +84,18 @@ mod tests {
     use super::*;
 
     const EXAMPLE: &str = indoc::indoc! {"
-        REPLACE_ME
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
 "};
 
     #[test]
     fn example() {
-        assert_eq!(part1(EXAMPLE), 0);
-        assert_eq!(part2(EXAMPLE), 0);
+        assert_eq!(part1(EXAMPLE), 2);
+        assert_eq!(part2(EXAMPLE), 4);
     }
 
     #[test]
@@ -27,7 +103,7 @@ mod tests {
         let input = crate::utils::get_day_input!();
         let output = part1(&input);
         println!("Part 1: {}", output);
-        assert_eq!(output, 0);
+        assert_eq!(output, 359);
     }
 
     #[test]
@@ -35,6 +111,6 @@ mod tests {
         let input = crate::utils::get_day_input!();
         let output = part2(&input);
         println!("Part 2: {}", output);
-        assert_eq!(output, 0);
+        assert_eq!(output, 418);
     }
 }
